39a38f25803c410b15aef317021b7755c1897d9d
popovic-nedeljko <nftnedja@gmail.com>
2023-01-15 23:48:21 +0100

    Change root for React v18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import createRoot from 'react-dom';
 import './sass/main.scss';
 import App from './App';
 import { AppProvider } from './context';
-ReactDOM.render(
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
