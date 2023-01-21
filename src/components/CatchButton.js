cf4b19355e0c757142b5bddb7ff6436206159361
popovic-nedeljko <nftnedja@gmail.com>
2023-01-22 00:58:14 +0100

    Catch pokemon funcionaliti added

---
+++ src/components/CatchButton.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useGlobalContext } from '../context';
+import { MdCatchingPokemon } from 'react-icons/md';
+import Popup from './Popup';
+
+const CatchButton = () => {
+  const { pathname } = useLocation();
+  const pokeId = +pathname.slice(1);
+  const { catchPokemon, removePokemon, catchedPokemons, setModal } =
+    useGlobalContext();
+
+  return catchedPokemons.some((pokemon) => pokemon.id === pokeId) ? (
+    <button
+      onClick={removePokemon}
+      className='btn--ellipse btn--catch_pokemon btn--ellipse--animated'
+    >
+      <p>release pokemon</p>
+      <MdCatchingPokemon />
+    </button>
+  ) : (
+    <button
+      onClick={() => {
+        catchPokemon();
+        setModal(true);
+        setTimeout(() => {
+          setModal(false);
+        }, 1500);
+      }}
+      className='btn--ellipse btn--catch_pokemon btn--ellipse--animated'
+    >
+      <p>catch pokemon</p>
+      <MdCatchingPokemon />
+    </button>
+  );
+};
+
+export default CatchButton;
---
+++ src/components/CatchDetails.js
@@ -0,0 +1,24 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { CgPokemon } from 'react-icons/cg';
+import { useGlobalContext } from '../context';
+
+const CatchDetails = () => {
+  const { catchedPokemons } = useGlobalContext();
+
+  return (
+    <div className='pokemon__your-pokemon-details'>
+      <div className='pokemon__info'>
+        <CgPokemon />
+        <span className='pokemon__info-text'>You have: </span>
+        <span className='pokemon__info-data pokemon__info-data--pokemons'>
+          {catchedPokemons.length}
+        </span>
+        <span className='pokemon__info-text'>pokemons</span>
+      </div>
+    </div>
+  );
+};
+
+export default CatchDetails;
+
+// ${this._data.yourPokemons.length}
