8843e99dcc22ed07a68bcfadbd3b17df66250065
popovic-nedeljko <nftnedja@gmail.com>
2023-01-22 00:57:12 +0100

    Added footer and popup modal window

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../img/pokeball-icon.png';
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer'>
+        <div className='footer__logo-box'>
+          <picture className='footer__logo'>
+            <img alt='Full logo' src={logo} />
+          </picture>
+        </div>
+
+        <div className='footer__copyright'>
+          <p className='footer__copyright-paragraf'>
+            &copy; Copyright by
+            <a
+              className='ln-link'
+              target='_blank'
+              href='https://github.com/popovic-nedeljko'
+              rel='noreferrer'
+            >
+              Nedeljko Popovic
+            </a>
+            . Welcome to PokemonApp. Have a nice time browsing for your favorite
+            pokemons.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
