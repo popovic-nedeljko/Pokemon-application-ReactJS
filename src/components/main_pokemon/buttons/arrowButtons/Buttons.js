6f23141851aa531e42978e9554544c46697910ea
popovic-nedeljko <nftnedja@gmail.com>
2023-02-06 16:41:04 +0100

    update of buttons

--- src/components/main_pokemon/buttons/arrowButtons/Buttons.js
+++ src/components/main_pokemon/buttons/arrowButtons/Buttons.js
@@ -6,27 +6,29 @@ import {
 } from 'react-icons/bs';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../../../context';
-import { useState } from 'react';
 import { useEffect } from 'react';
 
 const ButtonsNextPrev = () => {
   const { pathname } = useLocation();
   const id = +pathname.slice(1);
   let navigate = useNavigate();
-  const pageNum = Math.ceil(id / 10);
   const { page, setPage, isSearched, pokemonData } = useGlobalContext();
+  const pageNum = Math.ceil(id / 10);
+
+  console.log(page);
+  console.log(pageNum);
 
   useEffect(() => {
     const handleKeyPress = (e) => {
       if (e.key === 'ArrowLeft') {
-        if (page === pageNum) {
+        if (pageNum === +page) {
           navigate(`${+id - 1}`);
         } else {
-          navigate(`${+id - 1}`);
           setPage(pageNum);
+          navigate(`${+id - 1}`);
         }
       } else if (e.key === 'ArrowRight') {
-        if (page === pageNum) {
+        if (pageNum === +page) {
           navigate(`${+id + 1}`);
         } else {
           setPage(pageNum);
@@ -38,18 +40,18 @@ const ButtonsNextPrev = () => {
     return () => {
       document.removeEventListener('keydown', handleKeyPress);
     };
-  }, [id]);
+  }, [page, id]);
 
   return (
     <div className='pokemon__info-buttons'>
       <button
         disabled={(isSearched && true) || (+id > 1 ? false : true)}
         onClick={() => {
-          if (page === pageNum) {
+          if (pageNum === +page) {
             navigate(`${+id - 1}`);
           } else {
-            navigate(`${+id - 1}`);
             setPage(pageNum);
+            navigate(`${+id - 1}`);
           }
         }}
         className='btn--next--prev btn--prev'
@@ -60,7 +62,7 @@ const ButtonsNextPrev = () => {
       <button
         disabled={(isSearched && true) || (!pokemonData && true)}
         onClick={() => {
-          if (page === pageNum) {
+          if (pageNum === +page) {
             navigate(`${+id + 1}`);
           } else {
             setPage(pageNum);
